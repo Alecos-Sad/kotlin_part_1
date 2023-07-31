2c1f5a4f417bd3f486aa09542edb7a6cc152aa96
Sadonikov Alexander <alecos100@gmail.com>
2023-07-31 20:14:06 +0300

    2. if-else

--- kotlin-book/src/main/kotlin/Main.kt
+++ kotlin-book/src/main/kotlin/Main.kt
@@ -1,20 +1,38 @@
-import java.util.Arrays
+const val HERO_NAME = "Madrigal"
 
 fun main() {
-    var hasStead = false
-    var money = 50
-    val nameTracktir = "Trackt"
-    val menu = Arrays.asList("Beer", "Milk", "Vodka")
     println("The hero announces her presence to the world.")
-    val HERO_NAME = "Madrigal"
     println(HERO_NAME)
     var playerLevel = 4
     println(playerLevel)
+    val hasBeFriendedBarbarians = true
+    val hasAngeredBarbarians = false
+    val playerClass = "paladin"
+    val quest = if (playerLevel == 1) {
+        "Meet Mr. Bubbles in the land of soft things."
+    } else if (playerLevel <= 5) {
+        val canTalkToBarbarians = !hasAngeredBarbarians && (hasBeFriendedBarbarians || playerClass == "barbarian")
+        if (canTalkToBarbarians) {
+            "Convince the barbarians to call off their invasion."
+        } else {
+            "Save the town from the barbarian invasions."
+        }
+    } else if (playerLevel == 6) {
+        "Locate the enchanted sword."
+    } else if (playerLevel == 7) {
+        "Recover the long-lost artifact of creation."
+    } else if (playerLevel == 8) {
+        "Defeat Nogartse, bringer of death and eater of worlds."
+    } else {
+        "There are no quests right now."
+    }
+    println("The hero approaches the bounty board. It reads: ")
+    println(quest)
 
-    println("The hero embarks on her 1 journey to locate the enchanted sword.")
-    playerLevel ++
+    println("Time passes...")
+    println("There are no quests right now.")
+
+    playerLevel += 1
     println(playerLevel)
 
-    val reversed = HERO_NAME.reversed()
-    println(reversed)
 }
