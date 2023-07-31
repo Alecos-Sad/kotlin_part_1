aaa8f276ba7c68e55c874dd2fea46e383cc327aa
SadovNick <alecos100@gmail.com>
2023-07-31 12:22:37 +0300

    Initial

---
+++ kotlin-book/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("The hero announces her presence to the world.")
+    val HERO_NAME = "Madrigal"
+    println(HERO_NAME)
+    var playerLevel = 4
+    println(playerLevel)
+
+    println("The hero embarks on her 1 journey to locate the enchanted sword.")
+    playerLevel ++
+    println(playerLevel)
+}
