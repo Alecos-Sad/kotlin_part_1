59564fc2931458e413359d067ae1492da9ad5517
SadovNick <alecos100@gmail.com>
2023-07-31 12:28:34 +0300

    1. Задания главы 1

--- kotlin-book/src/main/kotlin/Main.kt
+++ kotlin-book/src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
+import java.util.Arrays
+
 fun main() {
+    var hasStead = false
+    var money = 50
+    val nameTracktir = "Trackt"
+    val menu = Arrays.asList("Beer", "Milk", "Vodka")
     println("The hero announces her presence to the world.")
     val HERO_NAME = "Madrigal"
     println(HERO_NAME)
@@ -8,4 +14,7 @@ fun main() {
     println("The hero embarks on her 1 journey to locate the enchanted sword.")
     playerLevel ++
     println(playerLevel)
+
+    val reversed = HERO_NAME.reversed()
+    println(reversed)
 }
