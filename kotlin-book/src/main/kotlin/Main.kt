0ca5a127a8d819479112e511cae599f4162df9de
Sadonikov Alexander <alecos100@gmail.com>
2023-07-31 22:08:03 +0300

    5. Пишем свои функции.

--- kotlin-book/src/main/kotlin/Main.kt
+++ kotlin-book/src/main/kotlin/Main.kt
@@ -1,13 +1,34 @@
+
 const val HERO_NAME = "Madrigal"
+var playerLevel = 5;
 
 fun main() {
     println("The hero announces her presence to the world.")
     println(HERO_NAME)
-    var playerLevel = 4
     println(playerLevel)
-    val hasBeFriendedBarbarians = true
-    val hasAngeredBarbarians = false
-    val playerClass = "paladin"
+
+    readBountyBoard()
+
+    println("Time passes...")
+    println("There are no quests right now.")
+
+    playerLevel += 1
+    println(playerLevel)
+
+    readBountyBoard()
+}
+
+private fun readBountyBoard(){
+    println("The hero approaches the bounty board. It reads: ")
+    println(obtainQuest(playerLevel, "paladin", true, false))
+}
+
+private fun obtainQuest(
+    playerLevel: Int,
+    playerClass: String,
+    hasAngeredBarbarians: Boolean,
+    hasBeFriendedBarbarians: Boolean,
+): String {
     val quest = when (playerLevel) {
         1 -> "Meet Mr. Bubbles in the land of soft things."
         in 2..5 -> {
@@ -18,18 +39,11 @@ fun main() {
                 "Save the town from the barbarian invasions."
             }
         }
+
         6 -> "Locate the enchanted sword."
         7 -> "Recover the long-lost artifact of creation."
         8 -> "Defeat Nogartse, bringer of death and eater of worlds."
         else -> "There are no quests right now."
     }
-
-    println("The hero approaches the bounty board. It reads: ")
-    println(quest)
-
-    println("Time passes...")
-    println("There are no quests right now.")
-
-    playerLevel += 1
-    println(playerLevel)
+    return quest
 }
