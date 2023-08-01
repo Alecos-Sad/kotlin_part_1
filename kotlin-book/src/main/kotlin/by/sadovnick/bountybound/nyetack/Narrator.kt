5f6bfc9532d0858a6c19c20b96f2569b61bfd53f
SadovNick <alecos100@gmail.com>
2023-08-01 15:53:12 +0300

    24. Определение функции, которая получает функцию.

--- kotlin-book/src/main/kotlin/by/sadovnick/bountybound/nyetack/Narrator.kt
+++ kotlin-book/src/main/kotlin/by/sadovnick/bountybound/nyetack/Narrator.kt
@@ -1,10 +1,55 @@
 package by.sadovnick.bountybound.nyetack
 
-val narrationModifier: (String) -> String =  {
-    val numExclamationPoint = 3
-    it.uppercase() + "!".repeat(numExclamationPoint)
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+
+var narrationModifier: (String) -> String = {
+    it
 }
 
-fun narrate(message: String) {
-    println(narrationModifier(message))
+fun narrate(
+    message: String,
+    modifier: (String) -> String = {
+        narrationModifier(it)
+    }
+) {
+    println(modifier(message))
+}
+
+fun changeNarratorMood() {
+    val mood: String
+    val modifier: (String) -> String
+    when (Random.nextInt(1..4)) {
+        1 -> {
+            mood = "loud"
+            modifier = { message ->
+                val numExclamationPoints = 3
+                message.uppercase() + "!".repeat(numExclamationPoints)
+            }
+        }
+
+        2 -> {
+            mood = "tired"
+            modifier = { message ->
+                message.lowercase().replace(" ", "...")
+            }
+        }
+
+        3 -> {
+            mood = "unsure"
+            modifier = { message ->
+                "$message?"
+            }
+        }
+
+        else -> {
+            mood = "professional"
+            modifier = { message ->
+                "$message."
+            }
+        }
+    }
+    narrationModifier = modifier
+    narrate("The narrator begins to feel $mood")
 }
--- kotlin-book/src/main/kotlin/by/sadovnick/bountybound/nyetack/NyetHack.kt
+++ kotlin-book/src/main/kotlin/by/sadovnick/bountybound/nyetack/NyetHack.kt
@@ -1,6 +1,12 @@
 package by.sadovnick.bountybound.nyetack
 
 fun main() {
-    narrate("A hero enters the town of Kronstadt. What is their name?")
+    narrate("A hero enters the town of Kronstadt. What is their name?") {
+        //Выводит сообщение желтым цветом
+            message ->
+        "\u001b[33;1m$message\u001b[0m"
+    }
     val heroName = readLine() ?: ""
+    changeNarratorMood()
+    narrate("$heroName heads to the town square")
 }
