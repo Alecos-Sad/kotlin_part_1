3433f1b4428dd9307ed65fa1e126a45abde6ac79
SadovNick <alecos100@gmail.com>
2023-08-02 11:22:39 +0300

    29. Проверка содержимого списка.

--- kotlin-book/src/main/kotlin/by/sadovnick/bountybound/nyetack/Tavern.kt
+++ kotlin-book/src/main/kotlin/by/sadovnick/bountybound/nyetack/Tavern.kt
@@ -6,8 +6,16 @@ private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 fun visitTavern() {
     narrate("$heroName enters $TAVERN_NAME")
     val patrons = listOf("Eli", "Mordoc", "Sophie")
-    println(patrons[0])
-    println(patrons.first())
-    println(patrons.last())
-    println(patrons.getOrElse(4) { "Unknown Patron" })
+    val elliMessage = if (patrons.contains("Eli")) {
+        "$TAVERN_MASTER says: Eli's  in the back playing cards"
+    } else {
+        "$TAVERN_MASTER says: Eli isn't here"
+    }
+    println(elliMessage)
+    val otherMessage = if (patrons.containsAll(listOf("Mordoc", "Sophie"))) {
+        "$TAVERN_MASTER says: Sophie and Mordoc are seated by the stew kettle"
+    } else {
+        "$TAVERN_MASTER says: Sophie and Mordoc aren't with each other right now"
+    }
+    println(otherMessage)
 }
