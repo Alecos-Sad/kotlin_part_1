9dba4308a5e9ff42acf31b2e5f13257ddb8d9a08
SadovNick <alecos100@gmail.com>
2023-08-02 11:15:30 +0300

    28. Границы индексов и безопасное обращение к индексам

--- kotlin-book/src/main/kotlin/by/sadovnick/bountybound/nyetack/Tavern.kt
+++ kotlin-book/src/main/kotlin/by/sadovnick/bountybound/nyetack/Tavern.kt
@@ -3,8 +3,11 @@ package by.sadovnick.bountybound.nyetack
 private const val TAVERN_MASTER = "Taernyl"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 
-fun visitTavern(){
+fun visitTavern() {
     narrate("$heroName enters $TAVERN_NAME")
     val patrons = listOf("Eli", "Mordoc", "Sophie")
-    println(patrons)
+    println(patrons[0])
+    println(patrons.first())
+    println(patrons.last())
+    println(patrons.getOrElse(4) { "Unknown Patron" })
 }
