5ae13b7ef68ae6855e1f3575314ee8cb12777a83
Sadonikov Alexander <alecos100@gmail.com>
2023-08-02 22:12:32 +0300

    35. Добавление записей в ассоцоативный массив

--- kotlin-book/src/main/kotlin/by/sadovnick/nuetack/Tavern.kt
+++ kotlin-book/src/main/kotlin/by/sadovnick/nuetack/Tavern.kt
@@ -19,13 +19,16 @@ fun visitTavern() {
     narrate("There are several items for sale: ")
     narrate(menuItems.joinToString())
     val patrons: MutableSet<String> = mutableSetOf()
-    while (patrons.size < 10) {
-        patrons += "${firstNames.random()} ${lastNames.random()}"
-    }
-    val patronGold = mapOf(
+    val patronGold = mutableMapOf(
         TAVERN_MASTER to 86.00,
         heroName to 4.50
     )
+    while (patrons.size < 10) {
+        val patronName = "${firstNames.random()} ${lastNames.random()}"
+        patrons += patronName
+        patronGold += patronName to 6.0
+
+    }
     narrate("$heroName sees several patrons in the tavern: ")
     narrate(patrons.joinToString())
     repeat(3) {
