c50241a191ef4c7a66e4c17472e8ff7b3b6147f6
SadovNick <alecos100@gmail.com>
2023-08-02 09:47:05 +0300

    26. Ссылка на функцию.

--- kotlin-book/src/main/kotlin/by/sadovnick/bountybound/nyetack/NyetHack.kt
+++ kotlin-book/src/main/kotlin/by/sadovnick/bountybound/nyetack/NyetHack.kt
@@ -1,11 +1,12 @@
 package by.sadovnick.bountybound.nyetack
 
 fun main() {
-    narrate("A hero enters the town of Kronstadt. What is their name?") {
-        //Выводит сообщение желтым цветом
-            message ->
-        "\u001b[33;1m$message\u001b[0m"
-    }
+    narrate(
+        "A hero enters the town of Kronstadt. What is their name?",
+        ::makeYellow
+    )
+
+
     val heroName = readLine()
     require(!heroName.isNullOrEmpty()) {
         "The hero must have a name"
@@ -14,11 +15,13 @@ fun main() {
     narrate("$heroName, ${createTitle(heroName)}, heads to the town square")
 }
 
-private fun createTitle(name: String): String{
-    return when{
+private fun createTitle(name: String): String {
+    return when {
         name.all { it.isDigit() } -> "The Identifiable"
         name.none { it.isLetter() } -> "The Witness Protection Member"
         name.count { it.lowercase() in "aeiou" } > 4 -> "The Master of Vowel"
         else -> "The Renowned Hero"
     }
 }
+
+private fun makeYellow(message: String) = "\u001b[33;1m$message\u001b[0m"
