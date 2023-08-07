939bf6c22bb6c1a6ce2a37b15e893116c3861380
Sadonikov Alexander <alecos100@gmail.com>
2023-08-07 20:10:12 +0300

    64. Ко- и контрвариантность.

--- kotlin-book/src/main/kotlin/by/sadovnick/nuetack/Loot.kt
+++ kotlin-book/src/main/kotlin/by/sadovnick/nuetack/Loot.kt
@@ -1,14 +1,37 @@
 package by.sadovnick.nuetack
 
-class LootBox<T>(var contents: T)
+class LootBox<T : Loot>(val contents: T)
+
+class DropOffBox<T> where T : Loot, T : Sellable {
+    fun sellLoot(sellableLoot: T): Int {
+        return (sellableLoot.value * 0.7).toInt()
+    }
+}
+
+abstract class Loot {
+    abstract val name: String
+}
+
+interface Sellable {
+    val value: Int
+}
+
+abstract class Hat : Loot(), Sellable
 
 class Fedora(
-    val name: String,
-    val value: Int
-)
+    override val name: String,
+    override val value: Int
+) : Hat()
+
+class Fez(
+    override val name: String,
+    override val value: Int
+) : Hat()
 
 class GemStones(
-    val value: Int
-)
+    override val value: Int
+) : Loot(), Sellable {
+    override val name = "sack of gemstones worth $value gold"
+}
 
-class Key()
+class Key(override val name: String) : Loot()
