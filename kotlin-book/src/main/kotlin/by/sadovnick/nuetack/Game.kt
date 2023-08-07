62cae910220e8a53613df2bfad83d9c0448309c8
SadovNick <alecos100@gmail.com>
2023-08-07 09:31:01 +0300

    55. Вложенные классы.

--- kotlin-book/src/main/kotlin/by/sadovnick/nuetack/Game.kt
+++ kotlin-book/src/main/kotlin/by/sadovnick/nuetack/Game.kt
@@ -17,7 +17,17 @@ object Game {
             narrate("${player.name} of ${player.hometown}, ${player.title}, is in ${currentRoom.description()}")
             currentRoom.enterRoom()
             print("> Enter your command: ")
-            println("Last command: ${readLine()}")
+            GameInput(readLine()).processCommand()
+        }
+    }
+
+    private class GameInput(arg: String?) {
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1) { "" }
+
+        fun processCommand() = when (command.lowercase()) {
+            else -> narrate("I'm not sure what you're trying todo")
         }
     }
 }
